84238d7f3a19c5193aa97a9c3b8305d347ff4813
HellDima <d.ledevich@gmail.com>
2016-12-21 19:29:09 +0200

    road to ngrok

--- run.js
+++ run.js
@@ -9,21 +9,6 @@ var _ = require("lodash");
 var sleep = require('sleep');
 
 
-function promisedNoWait(cmd){
-    return new Promise(function(resolve, reject){
-        exec(cmd, function(error, stdout, stderr){
-            resolve("success");
-            console.log("hello")
-            if (error){
-                console.log("error")
-                reject(error);
-            }
-            console.log("successlog")
-            resolve("success");
-        })
-    })
-}
-
 function promisedExecPuts(cmd){
     return new Promise(function(resolve, reject){
         exec(cmd, function(error, stdout, stderr){
@@ -122,77 +107,83 @@ function promisedExecRemoveAdb(cmd){
 var co = require('co');
 
 co (function *(){
-    var myIp = yield promisedGetIp();
-    console.log('myip: '+myIp);
+    try {
+        var myIp = yield promisedGetIp();
+        console.log('myip: ' + myIp);
 
-    var url = "http://prtgapi.ironsrc.com/add_to_rproxy?username=circleci&password=ABFyeJQw6HzappNQ&ip="+myIp;
-    var options = {
-        method: 'POST',
-        url: url,
-        headers: {
-            'cache-control': 'no-cache',
-            'content-type': 'application/json'
+        var url = "http://prtgapi.ironsrc.com/add_to_rproxy?username=circleci&password=ABFyeJQw6HzappNQ&ip=" + myIp;
+        var options = {
+            method: 'POST',
+            url: url,
+            headers: {
+                'cache-control': 'no-cache',
+                'content-type': 'application/json'
+            }
+        };
+        var daniel = yield promisedRequest(options);
+        console.log(daniel);
+
+        console.log("Clear old adb key")
+        var removeAdbKeyOption = yield promisedExecRemoveAdb("awk '{print $1}' < ~/.android/adbkey.pub | openssl base64 -A -d -a | openssl md5 -c");
+        var removeAdbKeyRespond = yield promisedRequest(removeAdbKeyOption);
+        console.log(removeAdbKeyRespond)
+        sleep.sleep(10);
+
+        console.log("adb key")
+        var addAdbKeyOption = yield promisedExecAddAdb("awk '{print $1}' < ~/.android/adbkey.pub | openssl base64 -A -d -a | openssl md5 -c");
+        var addAdbKeyRespond = yield promisedRequest(addAdbKeyOption);
+        console.log(addAdbKeyRespond)
+        sleep.sleep(10);
+
+        console.log("run ngrok")
+        // var runNgrok = yield promisedNoWait("./ngrok http 8888 &")
+        exec("./ngrok http 8888 &", function () {
+        })
+        // console.log(runNgrok)
+
+        //Need to make Api requests to get available devices from STF
+
+        console.log("adb connect")
+        var adbConnect = yield promisedExecPuts("adb connect rproxy-il.ironsrc.com:7409");
+        // var adbConnect = yield promisedExecPuts("adb connect stf.ironsrc.com:7409");
+        console.log(adbConnect)
+
+        sleep.sleep(10)
+        console.log("adb devices")
+        var adbDevices = yield promisedExecPuts("adb devices");
+        console.log(adbDevices)
+
+        console.log("adb shell")
+        var shell_options = {
+            method: 'GET',
+            url: 'http://127.0.0.1:4040/api/tunnels',
+            headers: {'cache-control': 'no-cache'}
+        };
+        var shellRespond = yield promisedRequest(shell_options);
+        console.log(shellRespond)
+
+        var jsonObject = JSON.parse(shellRespond);
+        var arrayFound = _.filter(jsonObject.tunnels, function (val) {
+            if (val.proto === 'http') {
+                return val;
+            }
+        });
+        console.log(arrayFound[0].public_url);
+        var new_ip = arrayFound[0].public_url
+        // var new_ip = "http://www.walla.co.il"
+        var adbOpenBrowser = yield promisedExecPuts("adb shell am start -a android.intent.action.VIEW -d " + new_ip);
+        console.log(adbOpenBrowser)
+
+        sleep.sleep(10);
+        console.log("Clear old adb key")
+        removeAdbKeyOption = yield promisedExecRemoveAdb("awk '{print $1}' < ~/.android/adbkey.pub | openssl base64 -A -d -a | openssl md5 -c");
+        removeAdbKeyRespond = yield promisedRequest(removeAdbKeyOption);
+        console.log(removeAdbKeyRespond)
+
+        process.exit(0)
+    }catch (err){
+            console.log(err.stack)
         }
-    };
-    var daniel = yield promisedRequest(options);
-    console.log(daniel);
-
-    console.log("Clear old adb key")
-    var removeAdbKeyOption = yield promisedExecRemoveAdb("awk '{print $1}' < ~/.android/adbkey.pub | openssl base64 -A -d -a | openssl md5 -c");
-    var removeAdbKeyRespond = yield promisedRequest(removeAdbKeyOption);
-    console.log(removeAdbKeyRespond)
-    sleep.sleep(10);
-
-    console.log("adb key")
-    var addAdbKeyOption = yield promisedExecAddAdb("awk '{print $1}' < ~/.android/adbkey.pub | openssl base64 -A -d -a | openssl md5 -c");
-    var addAdbKeyRespond = yield promisedRequest(addAdbKeyOption);
-    console.log(addAdbKeyRespond)
-    sleep.sleep(10);
-
-    console.log("run ngrok")
-    // var runNgrok = yield promisedNoWait("./ngrok http 8888 &")
-    exec("./ngrok http 8888 &", function () {})
-    // console.log(runNgrok)
-
-    //Need to make Api requests to get available devices from STF
-
-    console.log("adb connect")
-    var adbConnect = yield promisedExecPuts("adb connect rproxy-il.ironsrc.com:7409");
-    // var adbConnect = yield promisedExecPuts("adb connect stf.ironsrc.com:7409");
-    console.log(adbConnect)
-
-    sleep.sleep(10)
-    console.log("adb devices")
-    var adbDevices = yield promisedExecPuts("adb devices");
-    console.log(adbDevices)
-
-    console.log("adb shell")
-    var shell_options = { method: 'GET',
-        url: 'http://127.0.0.1:4040/api/tunnels',
-        headers:
-        {'cache-control': 'no-cache' } };
-    var shellRespond = yield promisedRequest(shell_options);
-    console.log(shellRespond)
-
-    var jsonObject = JSON.parse(shellRespond);
-    var arrayFound = _.filter(jsonObject.tunnels, function(val){
-        if (val.proto === 'http'){
-            return val;
-        }
-    });
-    console.log(arrayFound[0].public_url);
-    var new_ip = arrayFound[0].public_url
-    // var new_ip = "http://www.walla.co.il"
-    var adbOpenBrowser = yield promisedExecPuts("adb shell am start -a android.intent.action.VIEW -d "+new_ip);
-    console.log(adbOpenBrowser)
-
-    sleep.sleep(10);
-    console.log("Clear old adb key")
-    removeAdbKeyOption = yield promisedExecRemoveAdb("awk '{print $1}' < ~/.android/adbkey.pub | openssl base64 -A -d -a | openssl md5 -c");
-    removeAdbKeyRespond = yield promisedRequest(removeAdbKeyOption);
-    console.log(removeAdbKeyRespond)
-
-    process.exit(0)
 
 });
 
